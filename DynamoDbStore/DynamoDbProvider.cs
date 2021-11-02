278928d506d40a5bde1722d528467402b45dbce3
premkumarh <premkumarh@outlook.com>
2021-11-02 23:33:25 +0530

    Boaring changes

--- DynamoDbStore/DynamoDbProvider.cs
+++ DynamoDbStore/DynamoDbProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DynamoDbStore
 {
-    public class DynamoDbProvider : IDynamoDbProvider
+    public class DynamoDbProvider : IDynamoDbReadProvider, IDynamoDbUpsertProvider, IDynamoDbDeleteProvider
     {
         private readonly IAmazonDynamoDB _dynamoDbClient;
         Table _table;
@@ -21,10 +21,10 @@ namespace DynamoDbStore
             _table = Table.LoadTable(_dynamoDbClient, tableName);
         }
 
-        public async Task<T> GetItemAsync<T>(string tableName, string key) where T : class
+        public async Task<T> GetItemAsync<T>(string tableName, string primaryKey) where T : class
         {
             LoadTable(tableName);
-            var data = await _table.GetItemAsync(key);
+            var data = await _table.GetItemAsync(primaryKey);
 
             if (data == null)
                 return null;
@@ -34,7 +34,21 @@ namespace DynamoDbStore
                 NullValueHandling = NullValueHandling.Ignore,
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore
             });
+        }
 
+        public async Task<T> GetItemAsync<T>(string tableName, string primaryKey, string sortKey) where T : class
+        {
+            LoadTable(tableName);
+            var data = await _table.GetItemAsync(primaryKey, sortKey);
+
+            if (data == null)
+                return null;
+
+            return JsonConvert.DeserializeObject<T>(data.ToJson(), new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
         }
 
         public async Task<T> PutItemAsync<T>(string tableName, T data) where T : class
@@ -51,16 +65,22 @@ namespace DynamoDbStore
             var documnet = Document.FromJson(JsonConvert.SerializeObject(data));
             var responseRecord = await _table.UpdateItemAsync(documnet);
             return JsonConvert.DeserializeObject<T>(responseRecord.ToJson());
-
         }
 
-        public async Task<T> UpdateItemAsync<T>(string tableName, string key, T data) where T : class
+        public async Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, T data) where T : class
         {
             LoadTable(tableName);
             var document = Document.FromJson(JsonConvert.SerializeObject(data));
-            var responseRecord = await _table.UpdateItemAsync(document, key);
+            var responseRecord = await _table.UpdateItemAsync(document, primaryKey);
             return JsonConvert.DeserializeObject<T>(responseRecord);
+        }
 
+        public async Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKery, T data) where T : class
+        {
+            LoadTable(tableName);
+            var document = Document.FromJson(JsonConvert.SerializeObject(data));
+            var responseRecord = await _table.UpdateItemAsync(document, primaryKey, sortKery);
+            return JsonConvert.DeserializeObject<T>(responseRecord);
         }
     }
 }
--- DynamoDbStore/IDynamoDbProvider.cs
+++ DynamoDbStore/IDynamoDbProvider.cs
@@ -7,11 +7,21 @@ using System.Threading.Tasks;
 
 namespace DynamoDbStore.Contract
 {
-    interface IDynamoDbProvider
+    interface IDynamoDbReadProvider
     {
-        Task<T> GetItemAsync<T>(string tableName, string key) where T : class;
+        Task<T> GetItemAsync<T>(string tableName, string primaryKey) where T : class;
+        Task<T> GetItemAsync<T>(string tableName, string primaryKey, string sortKey) where T : class;
+    }
+
+    interface IDynamoDbUpsertProvider
+    {
         Task<T> PutItemAsync<T>(string tableName, T data) where T : class;
         Task<T> UpdateItemAsync<T>(string tableName, T data) where T : class;
-        Task<T> UpdateItemAsync<T>(string tableName, string key, T data) where T : class;
+        Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, T data) where T : class;
+        Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKery, T data) where T : class;
+    }
+    interface IDynamoDbDeleteProvider
+    {
+
     }
 }
