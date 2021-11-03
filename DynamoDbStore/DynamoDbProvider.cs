02f2279600271346e91debf8c141af9194acc73f
premkumarh <premkumarh@outlook.com>
2021-11-04 01:27:52 +0530

    Added Few more GetItemAsync with GetItemConfiguration

--- DynamoDbStore/DynamoDbProvider.cs
+++ DynamoDbStore/DynamoDbProvider.cs
@@ -23,14 +23,6 @@ namespace DynamoDbStore
             _table = Table.LoadTable(_dynamoDbClient, tableName);
         }
 
-        /// <summary>
-        /// Initiates the asynchronous execution of the GetItem operation. Amazon.DynamoDBv2.DocumentModel.Table.GetItem
-        /// </summary>
-        /// <typeparam name="T">The type of element</typeparam>
-        /// <param name="tableName">Name of the table</param>
-        /// <param name="primaryKey">Primary key element of the document</param>
-        /// <param name="cancellationToken">Token which can be used to cancel the task</param>
-        /// <returns>The first element that matches the primary key</returns>
         public async Task<T> GetItemAsync<T>(string tableName, string primaryKey, CancellationToken cancellationToken = default) where T : class
         {
             LoadTable(tableName);
@@ -46,15 +38,6 @@ namespace DynamoDbStore
             });
         }
 
-        /// <summary>
-        /// Initiates the asynchronous execution of the GetItem operation. Amazon.DynamoDBv2.DocumentModel.Table.GetItem
-        /// </summary>
-        /// <typeparam name="T">The type of element</typeparam>
-        /// <param name="tableName">Name of the table</param>
-        /// <param name="primaryKey">Primary key element of the document</param>
-        /// <param name="sortKey">Sort key element of the document</param>
-        /// <param name="cancellationToken">Token which can be used to cancel the task</param>
-        /// <returns>The first element that matches the Hash key and Range key</returns>
         public async Task<T> GetItemAsync<T>(string tableName, string primaryKey, string sortKey, CancellationToken cancellationToken = default) where T : class
         {
             LoadTable(tableName);
@@ -70,39 +53,68 @@ namespace DynamoDbStore
             });
         }
 
-        /// <summary>
-        /// Initiates the asynchronous execution of the UpdateItem operation. Amazon.DynamoDBv2.DocumentModel.Table.UpdateItem
-        /// </summary>
-        /// <typeparam name="T">The type of element</typeparam>
-        /// <param name="tableName">Name of the table</param>
-        /// <param name="primaryKey">Primary key element of the document</param>
-        /// <param name="data">Object that needs to be updated in dynamodb</param>
-        /// <param name="cancellationToken">Token which can be used to cancel the task</param>
-        /// <returns>The updated data</returns>
-        public async Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, T data, CancellationToken cancellationToken = default) where T : class
+        public async Task<T> GetItemAsync<T>(string tableName, string primaryKey, bool consistentRead, List<string> attributesToGet = null, CancellationToken cancellationToken = default) where T:class
+        {
+            LoadTable(tableName);
+
+            GetItemOperationConfig getItemOperationConfig = new GetItemOperationConfig();
+            getItemOperationConfig.AttributesToGet = attributesToGet;
+            getItemOperationConfig.ConsistentRead = consistentRead;
+
+            var reponseData = await _table.GetItemAsync(primaryKey, getItemOperationConfig, cancellationToken);
+            return JsonConvert.DeserializeObject<T>(reponseData.ToJson());
+
+        }
+
+        public async Task<T> GetItemAsync<T>(string tableName, string primaryKey, string sortKey, bool consistentRead, List<string> attributesToGet = null, CancellationToken cancellationToken = default) where T : class
+        {
+            LoadTable(tableName);
+
+            GetItemOperationConfig getItemOperationConfig = new GetItemOperationConfig();
+            getItemOperationConfig.AttributesToGet = attributesToGet;
+            getItemOperationConfig.ConsistentRead = consistentRead;
+
+            var reponseData = await _table.GetItemAsync(primaryKey, sortKey, getItemOperationConfig, cancellationToken);
+            return JsonConvert.DeserializeObject<T>(reponseData.ToJson());
+
+        }
+
+        public async Task<bool> UpdateItemAsync<T>(string tableName, string primaryKey, T data, CancellationToken cancellationToken = default) where T : class
         {
             LoadTable(tableName);
             var document = Document.FromJson(JsonConvert.SerializeObject(data));
             var responseRecord = await _table.UpdateItemAsync(document, primaryKey);
-            return JsonConvert.DeserializeObject<T>(responseRecord);
+            return responseRecord != null;
         }
 
-        /// <summary>
-        /// Initiates the asynchronous execution of the UpdateItem operation. Amazon.DynamoDBv2.DocumentModel.Table.UpdateItem
-        /// </summary>
-        /// <typeparam name="T">The type of element</typeparam>
-        /// <param name="tableName">Name of the table</param>
-        /// <param name="primaryKey">Primary key element of the document</param>
-        /// <param name="sortKey">Sort key element of the document</param>
-        /// <param name="data">Object that needs to be updated in dynamodb</param>
-        /// <param name="cancellationToken">Token which can be used to cancel the task</param>
-        /// <returns>The updated data</returns>
-        public async Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKey, T data, CancellationToken cancellationToken = default) where T : class
+        public async Task<bool> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKey, T data, CancellationToken cancellationToken = default) where T : class
         {
             LoadTable(tableName);
             var document = Document.FromJson(JsonConvert.SerializeObject(data));
             var responseRecord = await _table.UpdateItemAsync(document, primaryKey, sortKey);
-            return JsonConvert.DeserializeObject<T>(responseRecord);
+            return responseRecord != null;
+        }
+    
+        public async Task<bool> PutItemAsync<T>(string tableName, T data, CancellationToken cancellationToken = default) where T : class
+        {
+            LoadTable(tableName);
+            var document = Document.FromJson(JsonConvert.SerializeObject(data));
+            var reponseData = await _table.PutItemAsync(document);
+            return reponseData != null;
+        }
+
+        public async Task<bool> DeleteItemAsync<T>(string tableName, string primaryKey, CancellationToken cancellationToken = default) where T : class
+        {
+            LoadTable(tableName);
+            var responseRecord = await _table.DeleteItemAsync(primaryKey, cancellationToken);
+            return responseRecord != null;
+        }
+
+        public async Task<bool> DeleteItemAsync<T>(string tableName, string primaryKey, string sortKey, CancellationToken cancellationToken = default) where T : class
+        {
+            LoadTable(tableName);
+            var responseRecord = await _table.DeleteItemAsync(primaryKey, sortKey, cancellationToken);
+            return responseRecord != null;
         }
     }
 }
--- DynamoDbStore/IDynamoDbReadProvider.cs
+++ DynamoDbStore/IDynamoDbReadProvider.cs
@@ -8,7 +8,7 @@ namespace DynamoDbStore.Contract
     {
         Task<T> GetItemAsync<T>(string tableName, string primaryKey, CancellationToken cancellationToken = default) where T : class;
         Task<T> GetItemAsync<T>(string tableName, string primaryKey, string sortKey, CancellationToken cancellationToken = default) where T : class;
-        Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, T data, CancellationToken cancellationToken = default) where T : class;
-        Task<T> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKey, T data, CancellationToken cancellationToken = default) where T : class;
+        Task<bool> UpdateItemAsync<T>(string tableName, string primaryKey, T data, CancellationToken cancellationToken = default) where T : class;
+        Task<bool> UpdateItemAsync<T>(string tableName, string primaryKey, string sortKey, T data, CancellationToken cancellationToken = default) where T : class;
     }
 }
